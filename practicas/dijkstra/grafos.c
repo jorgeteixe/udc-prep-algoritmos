c2793dd55494de9dc5a4314ee07e6ff37994ee14
Jorge Teixeira <jorge.teixeira@bizaway.com>
2022-01-10 13:47:53 +0100

    dijkstra funciona meu

--- practicas/dijkstra/grafos.c
+++ practicas/dijkstra/grafos.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
+#include <limits.h>
 
 #include "grafos.h"
 
+int min(int a, int b) {
+    return a < b ? a : b;
+}
 
 void dijkstra_normal(matriz m, int nodos, int *distancias) {
     int i, j, minx, mini;
@@ -20,7 +24,7 @@ void dijkstra_normal(matriz m, int nodos, int *distancias) {
     for (j = 1; j < nodos; j++) {
         // buscamos distancia mínima a un nodo no visitado
         // (camino especial mínimo)
-        minx = INT8_MAX;
+        minx = INT_MAX;
         for (i = 0; i < nodos; i++) {
             if (visitados[i] == 0 && distancias[i] < minx) {
                 minx = distancias[i];
@@ -29,18 +33,15 @@ void dijkstra_normal(matriz m, int nodos, int *distancias) {
         }
 
         // visitamos el mínimo
-        visitados[i] = 1;
+        visitados[mini] = 1;
 
         // calculamos mínimos
         for (i = 0; i < nodos; i++) {
             if (visitados[i] == 0) {
-                distancias[i] = distancias[i] < distancias[mini] + m[mini][i]
-                            ? distancias[i] 
-                            : distancias[mini] + m[mini][i];
+                distancias[i] = min(distancias[i],
+                                    distancias[mini] + m[mini][i]);
             }
         }
     }
-    // hago el free peta luego asi que algo hago mal, 
-    // da casi bien, es el ejemplo del ej2 que hicimos antes
-    // free(visitados);
+    free(visitados);
 }
